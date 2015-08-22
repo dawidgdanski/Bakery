90f3e0c23ec40dd8d7d329598f748e7a5eda78ee
Dawid Gdański <davidgdanski@gmail.com>
2015-08-22 18:19:02 +0200

    Renaming library module to godt-client module

--- app/src/main/res/raw/bakery_database.sql
+++ app/src/main/res/raw/bakery_database.sql
@@ -12,6 +12,7 @@ CREATE TABLE ingredient (
  recipe_id TEXT,
  ingredient_id TEXT,
  name TEXT,
+ UNIQUE(recipe_id, ingredient_id)
 )#
 
 CREATE TABLE element (
@@ -22,7 +23,51 @@ CREATE TABLE element (
  amount INTEGER,
  hint TEXT,
  unit_name TEXT,
- symbol TEXT
+ symbol TEXT,
+ UNIQUE(element_id, ingredient_id)
 )#
 
+CREATE TABLE recipe_with_ingredient (
+ _id INTEGER PRIMARY KEY AUTOINCREMENT,
+ recipe_id TEXT,
+  title TEXT,
+  description TEXT,
+  image_url TEXT,
+  ingredient_name TEXT
+)#
 
+CREATE VIRTUAL TABLE fts_recipe_with_ingredient (
+ recipe_id,
+ title,
+ description,
+ image_url,
+ ingredient_name
+)#
+
+CREATE TRIGGER insert_recipe_with_ingredient_after_recipe_insertion AFTER INSERT ON recipe
+ BEGIN
+ INSERT INTO
+ recipe_with_ingredient(recipe_id, title, description, image_url, ingredient_name)
+ VALUES(NEW.recipe_id, NEW.title, NEW.description, NEW.image_url, NULL);
+ END#
+
+ CREATE TRIGGER insert_fts_recipe_with_ingredient_after_recipe_insertion AFTER INSERT ON recipe
+  BEGIN
+  INSERT INTO
+  fts_recipe_with_ingredient(recipe_id, title, description, image_url, ingredient_name)
+  VALUES(NEW.recipe_id, NEW.title, NEW.description, NEW.image_url, NULL);
+  END#
+
+CREATE TRIGGER update_recipe_with_ingredient_after_ingredient_insertion AFTER INSERT ON ingredient
+ BEGIN
+ UPDATE recipe_with_ingredient
+ SET ingredient_name = NEW.name
+ WHERE recipe_id = NEW.recipe_id;
+ END#
+
+CREATE TRIGGER update_fts_recipe_with_ingredient_after_ingredient_insertion AFTER INSERT ON ingredient
+ BEGIN
+ UPDATE fts_recipe_with_ingredient
+ SET ingredient_name = NEW.name
+ WHERE recipe_id = NEW.recipe_id;
+ END#
---
+++ godt-client-tests/src/test/java/pl/dawidgdanski/bakery/library/model/IngredientTest.java
@@ -0,0 +1,15 @@
+package pl.dawidgdanski.bakery.library.model;
+
+import android.os.Build;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = Build.VERSION_CODES.LOLLIPOP, manifest = Config.NONE)
+public class IngredientTest {
+
+
+
+}
